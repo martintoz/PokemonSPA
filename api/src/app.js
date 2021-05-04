94d7ab73b3762b30b9e7843b26a8bb7460030445
Martin Tozer <martintoz@gmail.com>
2021-05-04 21:05:12 +0200

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -11,9 +11,11 @@ var all = require('./routes/all.js')
 require('./db.js');
 
 const server = express();
+const cors = require('cors')
 
 server.name = 'API';
 
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
